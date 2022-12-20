6648d06c2571c600452d47ce082b34d9bbd5792e
dikamuharman <dika1254@gmail.com>
2022-12-20 15:46:01 +0700

    Revert "feat: :heavy_plus_sign: setup tailwind css and add react icons"
    This reverts commit a3752fb3c2df5746888ce893389e810cd941ff89.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Provider from './provider';
 
 export default function RootLayout({
   children,
@@ -12,7 +13,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="bg-[#F9F9FB] relative min-h-[100vh]">{children}</body>
+      <body>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
