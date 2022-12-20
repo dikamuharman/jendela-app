a51a7a35f08b036f6cb5ae4beb4ea341499aeda7
dikamuharman <dika1254@gmail.com>
2022-12-20 21:51:02 +0700

    feat(desktop): :sparkles: create navbar for desktop

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,51 +1,44 @@
 'use client';
 import Link from 'next/link';
-import { useState } from 'react';
-import { FiAlignRight, FiX } from 'react-icons/fi';
 import JLogo from '../logo/logo';
+import JDrawer from './drawer';
 
 const JNavbar = () => {
-  const [openDrawer, setOpenDrawer] = useState(false);
-
   return (
     <>
-      <header className="max-w-[768px] w-full px-4 mx-auto pt-3">
-        <JLogo withTitle />
-      </header>
-      <button
-        className="w-[55px] h-[55px] bg-white shadow-sm grid place-content-center rounded-xl absolute bottom-[50px] right-4"
-        onClick={() => setOpenDrawer(true)}
-      >
-        <FiAlignRight size={24} color="#545E6B" />
-      </button>
-      <div
-        className={`top-0 right-0 w-[100vw] bg-white pl-8 pt-8  pr-4 text-netural-90 fixed h-full flex flex-col justify-between items-end ease-in-out duration-300 ${
-          openDrawer ? 'translate-x-0 ' : 'translate-x-full'
-        }`}
-      >
-        <JLogo />
-        <div className="mb-11">
-          <div className="flex gap-3 flex-col items-end w-full">
-            <Link href="/">Home</Link>
-            <Link href="/">Program studi</Link>
-            <Link href="/">Cara kontribusi</Link>
+      <header className="max-w-[1024px] w-full px-4 mx-auto pt-3">
+        <nav className="md:bg-white bg-transparent rounded-xl px-4 py-2 flex flex-col lg:flex-row justify-between ">
+          <JLogo withTitle />
+          <div className="md:flex gap-4 items-center hidden self-end">
+            <Link
+              href="/"
+              className="px-4 py-2 text-sm hover:underline text-netural-60 hover:text-netural-80 transition-color"
+            >
+              Home
+            </Link>
+            <Link
+              href="/"
+              className="px-4 py-2 text-sm hover:underline text-netural-60 hover:text-netural-80 transition-color"
+            >
+              Program studi
+            </Link>
+            <Link
+              href="/"
+              className="px-4 py-2 text-sm hover:underline text-netural-60 hover:text-netural-80 transition-color"
+            >
+              Cara kontribusi
+            </Link>
             <Link
               href="/"
               passHref
-              className="bg-primary-50 px-4 py-2 text-white hover:bg-primary-60 transition-colors rounded-lg"
+              className="bg-primary-50 px-6 py-2 text-white hover:bg-primary-60 transition-colors rounded-lg"
             >
               Login
             </Link>
           </div>
-          <div className="h-[150px]"></div>
-        </div>
-        <button
-          className="w-[55px] h-[55px] bg-white shadow-sm grid place-content-center rounded-xl absolute bottom-[50px] right-4"
-          onClick={() => setOpenDrawer(false)}
-        >
-          <FiX size={24} color="#545E6B" />
-        </button>
-      </div>
+        </nav>
+      </header>
+      <JDrawer />
     </>
   );
 };
